63ce1dc8a35c57a0028eb333911d9e845acb5b8b
NoamElisha <noampro21@gmail.com>
2025-09-18 13:16:18 +0300

    working on UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './Header'
-import Body from './Body'
+
+import Header from './Header';
+import Body from './Body';
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-     <>
+    <>
       <Header />
       <Body />
-
     </>
-  )
+  );
 }
-
-export default App
